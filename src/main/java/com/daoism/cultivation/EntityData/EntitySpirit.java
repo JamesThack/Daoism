138021e8fb6a9e0cde0d61bff655fd81b4618f3c
CalebGitBritannia <calebbritannia@gmail.com>
2019-07-03 13:19:57 +0100

    Entities now drop item cores with power based on max health

--- src/main/java/com/daoism/cultivation/EntityData/EntitySpirit.java
+++ src/main/java/com/daoism/cultivation/EntityData/EntitySpirit.java
@@ -1,20 +1,54 @@
 package com.daoism.cultivation.EntityData;
 
+import com.daoism.cultivation.API.ItemMethods;
 import com.daoism.cultivation.API.PlayerMethods;
+import com.daoism.cultivation.Registration.ItemInit;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.monster.EntitySilverfish;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EntitySpirit extends EntityZombie {
+
+    private List<ItemStack> stack;
 
     public EntitySpirit(World worldIn) {
         super(worldIn);
+        stack = new ArrayList<>();
     }
+
+    @Override
+    public void setDropItemsWhenDead(boolean dropWhenDead) {
+        super.setDropItemsWhenDead(dropWhenDead);
+    }
+
+    public void addDrop(ItemStack items) {
+        stack.add(items);
+    }
+
+    @Override
+    public void onDeath(DamageSource cause) {
+        for (ItemStack stack : stack) {
+            this.entityDropItem(stack, 0);
+        }
+
+    }
+
+    public void setMaxHealth(int health) {
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).applyModifier(new AttributeModifier("Ye", health, 1));
+}
+
+
 }
--- src/main/java/com/daoism/cultivation/EventsClass.java
+++ src/main/java/com/daoism/cultivation/EventsClass.java
@@ -2,17 +2,22 @@ package com.daoism.cultivation;
 
 import com.daoism.cultivation.API.ItemMethods;
 import com.daoism.cultivation.API.PlayerMethods;
+import com.daoism.cultivation.EntityData.EntitySpirit;
 import com.daoism.cultivation.ReadWrite.Entity.CultivationCapability;
 import com.daoism.cultivation.ReadWrite.Entity.CultivationHandler;
 import com.daoism.cultivation.ReadWrite.item.CoreHandler;
 import com.daoism.cultivation.Registration.ItemInit;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -53,6 +58,22 @@ public class EventsClass {
             e.addCapability(new ResourceLocation(Daoism.MODID, Daoism.NAME), new CoreHandler());
         }
 
+    }
+
+    @SubscribeEvent
+    public void onEntityDeath(LivingDeathEvent e) {
+        if(!e.getEntityLiving().getEntityWorld().isRemote) {
+            BlockPos pos = e.getEntityLiving().getPosition();
+            //EntitySpirit entity = new EntitySpirit(e.getEntityLiving().getEntityWorld());
+            //e.getEntityLiving().getEntityWorld().spawnEntity(entity);
+            //entity.setMaxHealth(1);
+            //entity.setPositionAndUpdate(pos.getX(), pos.getY(), pos.getZ());
+            ItemStack item = new ItemStack(ItemInit.GOLDEN_CORE, 1);
+            ItemMethods.setLevel(item, (int) e.getEntityLiving().getMaxHealth() * 2);
+            item.setStackDisplayName( ("Golden Core Level " + (int) e.getEntityLiving().getMaxHealth() * 2) );
+            EntityItem drop = new EntityItem(e.getEntityLiving().getEntityWorld(), pos.getX(), pos.getY(), pos.getZ(), item);
+            e.getEntityLiving().getEntityWorld().spawnEntity(drop);
+        }
     }
 
 }
