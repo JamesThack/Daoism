36b8d9cfab2f930f4f71f84e3ae4e23ec97730ba
CalebGitBritannia <calebbritannia@gmail.com>
2019-07-13 13:28:42 +0100

    Started working on flying swords

---
+++ src/main/java/com/daoism/cultivation/ItemBlockData/Sword/FlyingSword.java
@@ -0,0 +1,41 @@
+package com.daoism.cultivation.ItemBlockData.Sword;
+
+import com.daoism.cultivation.API.PlayerMethods;
+import com.daoism.cultivation.Registration.ItemBase;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+public class FlyingSword extends ItemBase {
+    /**
+     * Constructor, sets the unlocalised name and registers it
+     *
+     * @param name Unlocalised name
+     */
+    public FlyingSword(String name) {
+        super(name);
+        this.setMaxStackSize(1);
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+        if(!worldIn.isRemote) {
+                Vec3d lookVec = playerIn.getLookVec();
+                double maxer = 4000;
+                int topper = 8000;
+                double x = ((lookVec.x * 0.3) * (PlayerMethods.getEntityCultivationLevel(playerIn, topper) / maxer));
+                double y = (((lookVec.y + 0.5) * 0.6) * (PlayerMethods.getEntityCultivationLevel(playerIn, topper) / maxer));
+                double z = ((lookVec.z * 0.3) * (PlayerMethods.getEntityCultivationLevel(playerIn, topper) / maxer));
+                playerIn.fallDistance = 0;
+                playerIn.setVelocity(x, y, z);
+
+        }
+        return new ActionResult<>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+    }
+}
