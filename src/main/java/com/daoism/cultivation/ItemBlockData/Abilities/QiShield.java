046deffcf98fb49860e83faed52d5fb2dd642d20
CalebGitBritannia <calebbritannia@gmail.com>
2020-01-08 01:01:43 +0000

    Added the ability to detect what block the player is looking at

--- src/main/java/com/daoism/cultivation/ItemBlockData/Abilities/QiShield.java
+++ src/main/java/com/daoism/cultivation/ItemBlockData/Abilities/QiShield.java
@@ -1,6 +1,14 @@
 package com.daoism.cultivation.ItemBlockData.Abilities;
 
+import com.daoism.cultivation.API.PlayerMethods;
 import com.daoism.cultivation.Registration.ItemBase;
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
 
 public class QiShield extends ItemBase {
     /**
@@ -11,5 +19,18 @@ public class QiShield extends ItemBase {
     public QiShield(String name) {
         super(name);
         this.setMaxStackSize(1);
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer player, EnumHand handIn) {
+        ItemStack curItem = player.getHeldItem(handIn);
+        if (!worldIn.isRemote) {
+            Block block = PlayerMethods.blockPlayerIsLookingAt(player, 3000);
+            System.out.println("Got here");
+            if (block !=null) {
+                System.out.println(block.getLocalizedName());
+            }
+        }
+        return new ActionResult<>(EnumActionResult.SUCCESS, curItem);
     }
 }
